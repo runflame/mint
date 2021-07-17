cf59ce7972825d3c87f3a72c80c5e9296dd38bc4
p0lunin <dmytro.polunin@gmail.com>
2021-07-17 11:10:51 +0300

    added inserting transactions into indexes and checks for reorgs

--- src/index.rs
+++ src/index.rs
@@ -1,38 +1,110 @@
 use bitcoincore_rpc::RpcApi;
-use bitcoin::{Transaction, BlockHash};
+use bitcoin::{Transaction, BlockHash, Txid, Block};
 use bitcoin::blockdata::script::Instruction;
 use bitcoin::blockdata::opcodes;
+use std::collections::HashMap;
+use std::cmp::Ordering;
+use bitcoincore_rpc::json::GetBlockHeaderResult;
 
-pub struct Tracker {
+pub struct Index {
     client: bitcoincore_rpc::Client,
+    tip: BlockHash,
+    blocks: u64,
+    checked_chain: Vec<BlockHash>,
+    index: HashMap<BlockHash, Vec<BitcoinMintOutput>>,
 }
 
-impl Tracker {
+impl Index {
     pub fn new(client: bitcoincore_rpc::Client) -> Self {
-        Tracker { client }
+        let info = client.get_blockchain_info().unwrap();
+        let tip = info.best_block_hash;
+        let blocks = info.blocks;
+        let checked_chain = Vec::new();
+        let index = HashMap::new();
+        Index { client, tip, blocks, checked_chain, index }
     }
 
-    /// Returns sidechain transactions indexes for giving bitcoin block id.
-    pub fn check_bitcoin_block_with_id(&self, id: u64) -> Vec<BitcoinMintOutput> {
+    pub fn check_last_blocks(&mut self) {
+        let new_blockchain_info = self.client.get_blockchain_info().unwrap();
+        match self.blocks.cmp(&new_blockchain_info.blocks) {
+            Ordering::Equal => return,
+            Ordering::Greater => unimplemented!("TODO: is it possible that chain will be less than previous?"),
+            Ordering::Less => {
+                let reorg_info = self.check_for_reorgs();
+                let old_height = match reorg_info {
+                    Some(reorg_info) => {
+                        for discarded_block in reorg_info.discarded_blocks {
+                            self.index.remove(&discarded_block).expect("Discarded block hashes must be given from the index");
+                        }
+                        let fork_position_in_checked_chain = self.checked_chain.len()-reorg_info.discarded_blocks.len();
+                        self.checked_chain.drain(..fork_position_in_checked_chain).collect();
+                        reorg_info.height_when_fork
+                    }
+                    None => self.blocks
+                };
+                for index in old_height+1..new_blockchain_info.blocks+1 {
+                    let hash = self.client.get_block_hash(index).unwrap();
+                    self.add_next_block_to_index(hash);
+                }
+            }
+        }
+    }
+
+    fn check_for_reorgs(&self) -> Option<ReorgInfo> {
+        let last_tip_block = self.client.get_block_header_info(&self.tip).unwrap();
+        if is_block_in_main_chain(&last_tip_block) {
+            return None;
+        }
+        let mut discarded_blocks = vec![self.tip];
+        for block_hash in self.checked_chain.iter().rev().skip(1) {
+            let block_header_info = self.client.get_block_header_info(&block_hash).unwrap();
+            if is_block_in_main_chain(&block_header_info) {
+                break;
+            }
+            else {
+                discarded_blocks.push(block_header_info.hash);
+            }
+        }
+        let height_when_fork = self.blocks - discarded_blocks.len() as u64;
+        Some(ReorgInfo {
+            height_when_fork,
+            discarded_blocks,
+        })
+    }
+
+    fn add_next_block_to_index(&mut self, block_hash: BlockHash) {
+        let transactions = self.check_bitcoin_block_with_hash(block_hash.clone());
+        if transactions.len() != 0 {
+            self.index.insert(block_hash, transactions);
+        }
+    }
+
+    fn check_bitcoin_block_with_id(&self, id: u64) -> Vec<BitcoinMintOutput> {
         let hash = self.client.get_block_hash(id).unwrap();
         self.check_bitcoin_block_with_hash(hash)
     }
 
-    /// Returns sidechain transactions indexes for giving bitcoin block hash.
-    pub fn check_bitcoin_block_with_hash(&self, hash: BlockHash) -> Vec<BitcoinMintOutput> {
+    fn check_bitcoin_block_with_hash(&self, hash: BlockHash) -> Vec<BitcoinMintOutput> {
         let block = self.client.get_block(&hash).unwrap();
         let txs = block.txdata;
 
-        let mint_txs = txs.into_iter().enumerate().filter_map(|(tx_pos, tx)| {
-            parse_mint_transaction(&hash, tx_pos as u64, tx)
-        }).collect();
+        let mint_txs = txs.into_iter().filter_map(parse_mint_transaction).collect();
 
         mint_txs
     }
 }
 
-pub fn parse_mint_transaction(block_hash: &BlockHash, tx_pos: u64, tx: Transaction) -> Option<BitcoinMintOutput> {
-    // TODO: Can be multiply OP_RETURN outputs?
+fn is_block_in_main_chain(block: &GetBlockHeaderResult) -> bool {
+    block.confirmations != -1
+}
+
+struct ReorgInfo {
+    height_when_fork: u64,
+    discarded_blocks: Vec<BlockHash>,
+}
+
+pub fn parse_mint_transaction(tx: Transaction) -> Option<BitcoinMintOutput> {
+    let txid = tx.txid();
     tx.output.iter().enumerate().filter_map(|(out_pos, out)| {
         let mut instructions = out.script_pubkey.instructions();
         let first_instruction = instructions.next().and_then(|res| res.ok());
@@ -47,8 +119,7 @@ pub fn parse_mint_transaction(block_hash: &BlockHash, tx_pos: u64, tx: Transacti
                 let bytes = Box::<[u8]>::from(bytes);
                 Some(BitcoinMintOutput {
                     index: BitcoinMintOutputIndex {
-                        block_hash: block_hash.clone(),
-                        transaction_position: tx_pos,
+                        txid,
                         output_position: out_pos as u64,
                     },
                     amount,
@@ -67,7 +138,6 @@ pub struct BitcoinMintOutput {
 }
 
 pub struct BitcoinMintOutputIndex {
-    block_hash: BlockHash,
-    transaction_position: u64,
+    txid: Txid,
     output_position: u64,
 }
