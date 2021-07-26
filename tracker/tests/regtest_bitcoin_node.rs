0a2f6190d517101ea1d53e5420e4935ed00388d2
p0lunin <dmytro.polunin@gmail.com>
2021-07-26 12:41:55 +0300

    prettified bitcoin node test

--- tracker/tests/regtest_bitcoin_node.rs
+++ tracker/tests/regtest_bitcoin_node.rs
@@ -1,21 +1,41 @@
-use bitcoin::blockdata::script;
-use bitcoin::consensus::Encodable;
 use bitcoin::hashes::hex::ToHex;
 use bitcoin::hashes::sha256d::Hash;
-use bitcoin::{BlockHash, Transaction, TxOut};
+use bitcoin::{Address, BlockHash, Txid};
 use bitcoincore_rpc::{Auth, Client, RpcApi};
 use serde_json::Value;
-use std::process::Stdio;
+use std::process::{Child, Stdio};
 use std::str::FromStr;
 use std::time::Duration;
 use tracker::bitcoin_client::BitcoinMintExt;
 use tracker::Index;
 
+const GENERATED_BLOCKS: u64 = 120;
+
 #[test]
 fn test_new_blocks_with_mint_txs() {
+    let (_dir, _child, client, address) = init_client();
+
+    // create mint transaction
+    let tx_id = client.send_mint_transaction(10, &[1; 32]).unwrap();
+    let mint_block = generate_block(&client, &address, &tx_id);
+
+    let mut index = Index::new(client, Some(119));
+    index.add_bag([1; 32]);
+
+    index.check_last_blocks();
+    assert_eq!(index.checked_height(), GENERATED_BLOCKS + 1);
+
+    let txs = index.get_index();
+    assert_eq!(txs.len(), 1); // we have only one mint transaction
+
+    let txs1 = txs.get(&mint_block).unwrap();
+    assert_eq!(txs1.last().unwrap().bag_id, [1; 32]);
+}
+
+fn init_client() -> (TempDir, Child, Client, Address) {
     const DIR: &'static str = "/tmp/bitcoin_test_node";
-    let _dir = TempDir::new(DIR.to_string());
-    let _node = setup_bitcoin_node(18444, 12001, DIR);
+    let dir = TempDir::new(DIR.to_string());
+    let node = setup_bitcoin_node(18444, 12001, DIR);
     std::thread::sleep(Duration::from_secs(1)); // Wait while node will started
     let client = Client::new(
         "http://localhost:12001".to_string(),
@@ -28,45 +48,14 @@ fn test_new_blocks_with_mint_txs() {
         .unwrap();
 
     let address = client.get_new_address(Some("testwal"), None).unwrap();
-    client.generate_to_address(120, &address).unwrap();
-
-    let tx_id = client.send_mint_transaction(10, &[1; 32]).unwrap();
-
-    let value: Value = client
-        .call(
-            "generateblock",
-            &[
-                Value::String(address.to_string()),
-                Value::Array(vec![Value::String(tx_id.as_hash().to_hex())]),
-            ],
-        )
+    client
+        .generate_to_address(GENERATED_BLOCKS, &address)
         .unwrap();
 
-    let hash = match value {
-        Value::Object(m) => match m.get("hash").unwrap() {
-            Value::String(hash) => hash.clone(),
-            _ => unreachable!(),
-        },
-        _ => unreachable!(),
-    };
-
-    let mint_block = BlockHash::from_hash(Hash::from_str(&hash).unwrap());
-
-    let mut index = Index::new(client, Some(119));
-    index.add_bag([1; 32]);
-
-    index.check_last_blocks();
-    assert_eq!(index.checked_height(), 121);
-
-    let txs = index.get_index();
-
-    assert_eq!(txs.len(), 1);
-
-    let txs1 = txs.get(&mint_block).unwrap();
-    assert_eq!(txs1.last().unwrap().bag_id, [1; 32])
+    (dir, node, client, address)
 }
 
-pub fn setup_bitcoin_node(port: u32, rpcport: u32, datadir: &str) -> std::process::Child {
+fn setup_bitcoin_node(port: u32, rpcport: u32, datadir: &str) -> Child {
     let child = std::process::Command::new("bash")
         .arg("./tests/setup_single_node.sh")
         .arg(port.to_string())
@@ -96,4 +85,26 @@ impl Drop for TempDir {
     fn drop(&mut self) {
         std::fs::remove_dir_all(self.path.as_str()).unwrap_or_else(|_| ());
     }
+}
+
+fn generate_block(client: &Client, address: &Address, tx_id: &Txid) -> BlockHash {
+    let value: Value = client
+        .call(
+            "generateblock",
+            &[
+                Value::String(address.to_string()),
+                Value::Array(vec![Value::String(tx_id.as_hash().to_hex())]),
+            ],
+        )
+        .unwrap();
+
+    let hash = match value {
+        Value::Object(m) => match m.get("hash").unwrap() {
+            Value::String(hash) => hash.clone(),
+            _ => unreachable!(),
+        },
+        _ => unreachable!(),
+    };
+
+    BlockHash::from_hash(Hash::from_str(&hash).unwrap())
 }
