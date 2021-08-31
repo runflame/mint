94c56bb59e24c4cd880602d7581c29d4a8b9eaf8
p0lunin <dmytro.polunin@gmail.com>
2021-08-31 11:28:39 +0300

    added docs how to use setup_single_node.sh

--- tracker/tests/setup_single_node.sh
+++ tracker/tests/setup_single_node.sh
@@ -1,3 +1,14 @@
+# Script is used to set up bitcoin node in regtest mode.
+# The bitcoin node will be started without output and its PID will be printed.
+# 3 arguments are required to install the script:
+# 1. Bitcoin node port. It is used for communication between nodes.
+# 2. Bitcoin node RPC port. It is used for RPC client.
+# 3. Directory where bitcoin node files will be stored.
+#
+# Examples how to run script:
+# > bash setup_single_node.sh 12001 18444 /data/dir
+# > bash setup_single_node.sh 12001 18444 (mktemp -d -t btc-node-XXXX)
+
 PORT=$1
 RPCPORT=$2
 BITCOIND=/usr/local/bin/bitcoind
